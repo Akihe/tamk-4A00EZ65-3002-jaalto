450d0e01411226097053d170c492e75e6be264bd
Akihe <aki.helin@tuni.fi>
2022-01-11 10:52:42 +0200

    Add 011 homework

---
+++ 011/Program.java
@@ -0,0 +1,14 @@
+public class Program {
+public static void main(String[] args) {
+
+    int repeat = 5;
+    String space = "";
+
+    for (int i = 0; i < repeat; i++) {
+        space += " ";
+        System.out.println(space + "*");
+    }
+
+    }
+}
+
