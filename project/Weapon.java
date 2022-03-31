07fb791258be8c2777c8ae15537f96f7197298a9
Akihe <aki.helin@tuni.fi>
2022-03-31 16:38:33 +0300

    Add abstract method starterWeaponIntro()

--- project/Weapon.java
+++ project/Weapon.java
@@ -2,7 +2,7 @@ package project;
 
 abstract class Weapon {
     private int damage;
-
+    abstract String starterWeaponIntro();
 
     public void setDamage(int damage) {
         this.damage = damage;
