915237b088d10759ac56871be6a763efe374067b
Akihe <aki.helin@tuni.fi>
2022-03-31 22:18:07 +0300

    remove empty line

--- project/Bow.java
+++ project/Bow.java
@@ -3,7 +3,6 @@ package project;
 public class Bow extends Weapon{
     private String weaponName = "Bow";
 
-
     @Override
     String starterWeaponIntro() {
         return "Here's your first one, the \"Bow of Many Arrows\" suits you very well for your first journey. Be careful!";
