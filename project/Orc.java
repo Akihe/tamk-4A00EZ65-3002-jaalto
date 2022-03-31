6cd92365ca638fa8c37e5346f374de5bc134f373
Akihe <aki.helin@tuni.fi>
2022-03-31 20:37:27 +0300

    implement some methods

--- project/Orc.java
+++ project/Orc.java
@@ -1,11 +1,25 @@
 package project;
 
-public class Orc extends BasicStatsAndSkills {
+public class Orc extends CharacterBase {
 
+    Orc(String name, int health, int manapoints, int damage) {
+        super(name, health, manapoints, damage);
+    }
+
+    @Override
+    int attack() {
+        return this.getDamage();
+
+    }
     @Override
     public void flee() {
         // TODO Auto-generated method stub
 
     }
 
+    @Override
+    void printAttacks() {
+        // TODO Auto-generated method stub
+
+    }
 }
