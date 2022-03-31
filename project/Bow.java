c3ea27bf24add167bc50f5d5ce6e8481140ee02e
Akihe <aki.helin@tuni.fi>
2022-03-31 16:34:33 +0300

    Create class Bow, extends Weapon

---
+++ project/Bow.java
@@ -0,0 +1,16 @@
+package project;
+
+public class Bow extends Weapon{
+    private String weaponName = "Bow";
+
+
+    @Override
+    String starterWeaponIntro() {
+        return "Here's your first one, the \"Bow of Many Arrows\" suits you very well for your first journey. Be careful!";
+    }
+
+    @Override
+    public String toString() {
+        return weaponName;
+    }
+}
