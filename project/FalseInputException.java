653d050e35a1c0756732a1643dfc21d23ef1df31
Akihe <aki.helin@tuni.fi>
2022-03-31 16:35:59 +0300

    create custom FalseInputException

---
+++ project/FalseInputException.java
@@ -0,0 +1,10 @@
+package project;
+
+/**
+ * WrongInputException
+ */
+public class FalseInputException extends Exception {
+    public FalseInputException(String message) {
+        super(message);
+    }
+}
