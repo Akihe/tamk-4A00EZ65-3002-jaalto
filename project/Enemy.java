9fd191077f90fe9b56bb5779f8b470c47ce429b3
Akihe <aki.helin@tuni.fi>
2022-03-31 22:18:59 +0300

    modify attack() and roar()

--- project/Enemy.java
+++ project/Enemy.java
@@ -7,14 +7,16 @@ public class Enemy extends CharacterBase implements EnemyBase {
     }
 
     @Override
-    int attack() {
-        return this.getDamage();
+    public int attack() {
+        System.out.println("Enemy punches you! It deals " + getDamage() + " damage!");
+        return getDamage();
 
     }
 
     @Override
     public int roar() {
-        return this.getDamage();
+        System.out.println("Enemy lets out a fierce roar! It deals " + getDamage() * 3 + " damage!");
+        return getDamage() * 3;
     }
 
     @Override
