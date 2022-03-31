75fc4b2be998da1901c7c1cd1b7533629f41ccf3
Akihe <aki.helin@tuni.fi>
2022-03-31 20:36:56 +0300

    call level one in Game class

--- project/Game.java
+++ project/Game.java
@@ -10,21 +10,21 @@ public class Game {
         createWeapon(weaponChoise);
         this.charName = charName;
         welcome();
+        LevelOne levelOne = new LevelOne(player, weapon);
     }
 
     public void welcome() {
         System.out.println(String.format("Welcome to the game %s! " +
                                          "Your choise of weapon seems to be a %s, great choise!%n%s", charName, weapon, weapon.starterWeaponIntro()));
-
     }
 
     public void createPlayer(int choise) {
         switch (choise) {
             case 1:
-                player = new Orc();
+                player = new Orc(charName, 100, 100, 10);
                 break;
             case 2:
-                player = new Human();
+                player = new Human(charName, 100, 100, 10);
                 break;
         }
     }
