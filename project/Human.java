033bf4fcba34b55bc084775b4d467983727dd78d
Akihe <aki.helin@tuni.fi>
2022-03-31 20:37:16 +0300

    implement some methods

--- project/Human.java
+++ project/Human.java
@@ -1,10 +1,24 @@
 package project;
 
-public class Human extends BasicStatsAndSkills {
+public class Human extends CharacterBase {
+
+    Human(String name, int health, int manapoints, int damage) {
+        super(name, health, manapoints, damage);
+    }
+
+    @Override
+    int attack() {
+        return this.getDamage();
+    }
 
     @Override
     public void flee() {
         // TODO Auto-generated method stub
 
+    }
+    @Override
+    void printAttacks() {
+        System.out.println("[1] Attack");
+        System.out.println("[2] Flee");
     }
 }
